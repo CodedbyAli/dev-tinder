5e8e3b0b20e9c27ae57ca45e95bae2a42184b286
Ali Hamza <quotedbyali@example.com>
2025-01-29 00:00:42 +0500

    Created user profile apis & add validation into schema level and api level

--- src/app.js
+++ src/app.js
@@ -4,19 +4,83 @@ const User = require('./models/User')
 
 const app = express();
 
+app.use(express.json());
+
+// SignUp
 app.post('/signup', async(req,res) => {
-  const user = new User ({
-    firstName: "Ali",
-    lastName: "Hamza",
-    email: "ali2@example.com",
-    password: "Ali123456"
-  });
+  const data = req.body;
 
   try{
-    await user.save();
+    await User.create(data);
     res.send("User created successfuly");
   } catch(err) {
-    res.status(400).send("Error while creating user");
+    res.status(400).send("Error while creating user: " + err.message);
+  }
+})
+
+// Get Users
+app.get('/users', async(req,res) => {
+  try{
+    const user = await User.find({});
+    res.send(user);
+  }catch(err){
+    res.status(404).send(err.message);
+  }
+});
+
+// Get User
+app.get('/user', async (req,res) => {
+  try{
+    
+    const user = await User.findOne({ email: 'ali2@example.com' });
+    res.send(user);
+
+  }catch(err){
+    res.status(400).send("Error Occur while fetching a User")
+  }
+})
+
+// Delete User
+app.delete('/user/:userId', async (req,res) => {
+  const {userId} = req.params;
+
+  try{
+    await User.deleteOne({ _id: userId });
+    res.send("User Deleted Successfully");
+  }catch(err){
+    res.status(400).send("Error while deleting user: " + err.message);
+  }
+})
+
+
+// Update User
+app.patch('/user/:userId', async (req,res) => {
+  const {userId} = req.params;
+  const data = req.body;
+
+  try{
+
+    const ALLOWED_UPDATES = ['age', 'gender', 'skills'];
+
+    const isUpdateAllow = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k));
+    if(!isUpdateAllow)
+    {
+      throw new Error("Update is not Allowed");
+    }
+
+    if(data?.skills?.length > 10)
+    {
+      throw new Error("Skills cannot be more than 10");
+    }
+
+    await User.findByIdAndUpdate({_id:userId}, data, {
+      returnDocument: "after",
+    });
+
+    res.send("User is updated successfully");
+
+  }catch(err) {
+    res.status(400).send("Error while updating user: " + err.message);
   }
 })
 
--- src/models/User.js
+++ src/models/User.js
@@ -1,25 +1,72 @@
 const mongoose = require('mongoose')
+const validator = require('validator');
 
 
 const userSchema = mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        min:3,
+        max:4
     },
     lastName: {
-        type: String
+        type: String,
+        required: true,
+        min:3,
+        max:4
     },
     email: {
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        validate: {
+            validator: function(v){
+                return validator.isEmail(v);
+            },
+            message: "Invalid email format"
+        }
     },
     password: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 6,
+        validate(value){
+            if(!validator.isStrongPassword(value))
+            {
+                throw new Error("Password is not valid: " + value);
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        min: 15,
+        max: 50
     },
     gender: {
-        type: String
+        type: String,
+        required: true,
+        // validate: {
+        //     validator: function(v){
+        //         return ['Male','Female','Other'].includes(v);
+        //     },
+        //     message: "Gender cannot be other than Male, Female and Other"
+        // }
+        validate(value){
+            if(!['Male','Female','Other'].includes(value))
+            {
+                throw new Error("Gender cannot be other than Male, Female and Other")
+            }
+        }
+    },
+    skills: {
+        type: Array,
+        required: true
     }
+},
+{
+    strict: true,
+    timestamps: true
 });
 
 
